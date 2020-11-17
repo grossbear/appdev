510a89d4b813e213b1c2bffaeca4ea4b33049617
grossbear <lichojwan@gmail.com>
2020-11-17 15:32:49 +0100

    Update msappwin.h

--- msappwin.h
+++ msappwin.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <windows.h>
 
+#define WIN_NAME_SIZE 256
+
 ///////////////////////////////////////////////////////////////////////////////
 class AppWindow
 {
 public:
-    AppWindow(int x, int y, int width, int height, const char * title);
+    AppWindow(int x, int y, int width, int height, int flags, const char * wname);
     ~AppWindow();
     bool WindowAvailable() const;
 
@@ -13,8 +15,10 @@ public:
 
     void ProcessEvents();
 
+    void SetTitle(const char * title);
+
 protected:
-    bool Create(int x, int y, int width, int height, const char * title);
+    bool Create(int x, int y, int width, int height, int flags);
     bool Destroy();
 
     HWND hWnd;
@@ -22,5 +26,7 @@ protected:
 private:
     bool RegisterWndClass(HINSTANCE hInst);
 
+    char winName[WIN_NAME_SIZE];
+    char winClassName[WIN_NAME_SIZE];
 };
 ///////////////////////////////////////////////////////////////////////////////
