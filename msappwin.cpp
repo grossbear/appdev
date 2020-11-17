f9f106d86fa7a3e208030484a70e43db425366b4
grossbear <lichojwan@gmail.com>
2020-11-17 15:32:23 +0100

    Update msappwin.cpp

--- msappwin.cpp
+++ msappwin.cpp
@@ -1,14 +1,40 @@
 #include <stdio.h>
 #include "msappwin.h"
 #include "appbase.h"
+#include "strutils.h"
 
 #define WIN_CLASS_NAME "AppWindowClass"
+#define WIN_CLASS_SUFFIX "Class"
+
+
+#define WIN_REG_FAILED_STR  "Window class registration failed!"
+#define WIN_ERROR_STR "Error!"
+#define WIN_CREATE_FAILED_STR "Window creation failed!"
+
+class WinParams
+{
+public:
+    bool active = false; // Window active flag
+    bool fullscreen = false; // Window fullscreen flag
+    bool blockScreensaver = false;
+    bool blockMonitorPowerSave = false;
+
+    bool closeOnEsc = true; // Close window on escape key
+};
+
+WinParams gWinParams;
 
 ///////////////////////////////////////////////////////////////////////////////
-AppWindow::AppWindow(int x, int y, int width, int height, const char * title):
-hWnd(NULL)
+AppWindow::AppWindow(int x, int y, int width, int height, int flags, const char * wname):
+hWnd(NULL) 
 {
-    Create(x, y, width, height, title);
+    memset(winName, 0, WIN_NAME_SIZE*sizeof(char));
+    memset(winClassName, 0, WIN_NAME_SIZE*sizeof(char));
+
+    strutils::copy(winName, wname, WIN_NAME_SIZE*sizeof(char));
+    strutils::cat(winClassName, WIN_CLASS_SUFFIX, sizeof(WIN_CLASS_SUFFIX));
+
+    Create(x, y, width, height, flags);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -27,28 +53,37 @@ bool AppWindow::WindowAvailable() const
 void AppWindow::ProcessEvents()
 {
     MSG msg;
-    if(PeekMessage(&msg,NULL,0,0,PM_REMOVE))    // Is There A Message Waiting?
+
+    // Is there a message waiting?
+    if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
     {
         //printf("Process Events\n");
-        TranslateMessage(&msg);                 // Translate The Message
-        DispatchMessage(&msg);                  // Dispatch The Message
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-bool AppWindow::Create(int x, int y, int width, int height, const char * title)
+void AppWindow::SetTitle(const char * title)
+{
+    SetWindowText(hWnd, title);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+bool AppWindow::Create(int x, int y, int width, int height, int flags)
 {
     HINSTANCE hInstance = GetModuleHandle(NULL);
     if (!RegisterWndClass(hInstance))
     {
-        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION|MB_OK);
+        UINT msgFlags = MB_ICONEXCLAMATION | MB_OK;
+        MessageBox(NULL, WIN_REG_FAILED_STR, WIN_ERROR_STR, msgFlags);
         return false;
     }
 
     DWORD dwStyle = WS_VISIBLE | WS_OVERLAPPEDWINDOW;
     hWnd = CreateWindowEx(WS_EX_CLIENTEDGE,
-        WIN_CLASS_NAME,
-        title,
+        winClassName,
+        winName,
         dwStyle,
 		x, //CW_USEDEFAULT
 		y, //CW_USEDEFAULT
@@ -58,11 +93,11 @@ bool AppWindow::Create(int x, int y, int width, int height, const char * title)
         NULL,
         hInstance,
         NULL);
-    return true;
 
     if (hWnd == NULL)
     {
-		MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION|MB_OK);
+        UINT msgFlags = MB_ICONEXCLAMATION | MB_OK;
+		MessageBox(NULL, WIN_CREATE_FAILED_STR, WIN_ERROR_STR, msgFlags);
 		return false;
 	}
 
@@ -70,6 +105,7 @@ bool AppWindow::Create(int x, int y, int width, int height, const char * title)
     ShowWindow(hWnd, SW_SHOW);
     SetForegroundWindow(hWnd);
     SetFocus(hWnd);
+    gWinParams.active = true;
 
     return true;
 }
@@ -81,7 +117,7 @@ bool AppWindow::Destroy()
     if(hWnd)
     {
         DestroyWindow( hWnd );
-        UnregisterClass( WIN_CLASS_NAME, hInstance );
+        UnregisterClass( winClassName, hInstance );
         return true;
     }
     return false;
@@ -95,13 +131,13 @@ bool AppWindow::RegisterWndClass(HINSTANCE hInstance)
 	memset(&wc,0,sizeof(wc));
 
     wc.cbSize       = sizeof(WNDCLASSEX);
-    wc.style        = CS_OWNDC;
+    wc.style        = CS_OWNDC | CS_DBLCLKS;
     // This is where we will send messages to
 	wc.lpfnWndProc  = AppWindow::WndProc;
 	wc.hInstance    = hInstance;
 	wc.hbrBackground= (HBRUSH)(COLOR_WINDOW+1);
                        //(HBRUSH) GetStockObject (BLACK_BRUSH);
-	wc.lpszClassName= WIN_CLASS_NAME;
+	wc.lpszClassName= winClassName;
     wc.hCursor      = LoadCursor(NULL, IDC_ARROW);
     // Load a standard icon
 	wc.hIcon        = LoadIcon(NULL, IDI_APPLICATION);
@@ -111,50 +147,181 @@ bool AppWindow::RegisterWndClass(HINSTANCE hInstance)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// hwnd - Handle for this window
+// message - Message for this Window
+// wParam - Additional message information
+// lParam - Additional message information
 LRESULT CALLBACK AppWindow::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     ApplicationBase * app = ApplicationBase::GetSingleton();
 
+    // Check for windows messages
     switch(message)
     {
+        // Watch for window activate message
+        case WM_ACTIVATE:
+        {
+            // Get minimization state
+            gWinParams.active = (bool)!HIWORD(wParam);
+            app->OnWindowActive(gWinParams.active);
+			return 0;		
+        }
+        break;
+
+        // Intercept System Commands
+        case WM_SYSCOMMAND:							
+		{
+            // Check System Calls
+			switch (wParam)							
+			{
+                // Screensaver Trying To Start?
+				case SC_SCREENSAVE:
+                    if(gWinParams.blockScreensaver)
+                        return 0; // Prevent From Happening
+                // Monitor Trying To Enter Powersave?			
+				case SC_MONITORPOWER:
+                    if(gWinParams.blockMonitorPowerSave)
+                        return 0; // Prevent From Happening
+			}
+		}
+        break;
+
         case WM_QUIT:
             printf("quit\n");
             //PostQuitMessage(0);
             //app->CloseWindow();
-            break;
+            return 0;
 
         case WM_CREATE:
             printf("create\n");
-            break;
+            return 0;
 
         case WM_SIZE:
-            app->SetWindowSize((int)LOWORD(lParam), (int)HIWORD(lParam));
-            break;
+        {
+            int x = (int)LOWORD(lParam);
+            int y = (int)HIWORD(lParam);
+            app->OnSetWindowSize(x, y);
+            return 0;
+        }
 
         case WM_DESTROY:
             printf("destroy\n");
-            break;
+            return 0;
 
+        // Did We Receive A Close Message?
         case WM_CLOSE:
             printf("close\n");
-            app->CloseWindow();
-            break;
+            app->OnCloseWindow();
+            return 0;
 
+        // Is A Key Being Held Down?
         case WM_KEYDOWN:
-            switch (wParam)
+        {
+            if(wParam == VK_ESCAPE && gWinParams.closeOnEsc)
             {
-            case VK_ESCAPE:
                 printf("escape\n");
                 PostQuitMessage(0);
                 return 0;
             }
-            return 0;
 
-        default:
-            return DefWindowProc (hwnd, message, wParam, lParam);
+            unsigned char key = (unsigned char) wParam;
+            app->OnKeyDown(key);
+            return 0;
+        }
+        break;
+
+        // Has A Key Been Released?
+        case WM_KEYUP:
+        {
+            unsigned char key = (unsigned char) wParam;
+            app->OnKeyUp(key);
+            return 0;
+        }
+        break;
+
+        // Left button operations
+        case WM_LBUTTONDOWN:
+        case WM_LBUTTONUP:
+        case WM_LBUTTONDBLCLK:
+        {
+            ButtonType btype = BTYPE_LBUTTON;
+            int x = (int) LOWORD(lParam);
+            int y = (int) HIWORD(lParam);
+            if(message == WM_LBUTTONDOWN)
+                app->OnMouseButtonDown(btype, x, y);
+            else if(message == WM_LBUTTONUP)
+                app->OnMouseButtonUp(btype, x, y);
+            else
+                app->OnMouseButtonDblClk(btype, x, y);
+            return 0;
+        }
+        break;
+
+        // Right button operations
+        case WM_RBUTTONDOWN:
+        case WM_RBUTTONUP:
+        case WM_RBUTTONDBLCLK:
+        {
+            ButtonType btype = BTYPE_RBUTTON;
+            int x = (int) LOWORD(lParam);
+            int y = (int) HIWORD(lParam);
+            if(message == WM_RBUTTONDOWN)
+                app->OnMouseButtonDown(btype, x, y);
+            else if (message == WM_RBUTTONUP)
+                app->OnMouseButtonUp(btype, x, y);
+            else
+                app->OnMouseButtonDblClk(btype, x, y);
+            return 0;
+        }
+        break;
+
+        // Middle button operations
+        case WM_MBUTTONDOWN:
+        case WM_MBUTTONUP:
+        case WM_MBUTTONDBLCLK:
+        {
+            ButtonType btype = BTYPE_MBUTTON;
+            int x = (int) LOWORD(lParam);
+            int y = (int) HIWORD(lParam);
+            if(message == WM_MBUTTONDOWN)
+                app->OnMouseButtonDown(btype, x, y);
+            else if(message == WM_MBUTTONUP)
+                app->OnMouseButtonUp(btype, x, y);
+            else
+                app->OnMouseButtonDblClk(btype, x, y);
+            return 0;
+        }
+        break;
+
+        // Mouse move operations
+        case WM_MOUSEMOVE:
+        {
+            int x = (int) LOWORD(lParam);
+            int y = (int) HIWORD(lParam);
+            app->OnMouseMove(x, y);
+       
+            switch(wParam)
+            {
+            case MK_LBUTTON:
+                app->OnMouseMove(BTYPE_LBUTTON, x, y);
+            break;
+
+            case MK_RBUTTON:
+                app->OnMouseMove(BTYPE_RBUTTON, x, y);
+                break;
+
+            case MK_MBUTTON:
+                app->OnMouseMove(BTYPE_MBUTTON, x, y);
+                break;
+            }
+            return 0;
+        }
+        break;
+
     }
 
-    return 0;
+    // Pass all unhandled messages to DefWindowProc
+    return DefWindowProc(hwnd, message, wParam, lParam);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
